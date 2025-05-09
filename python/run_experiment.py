993887e14af47ffbe1782f05335498b9043f59fc
henryrosehewitt@gmail.com <henryrosehewitt@gmail.com>
2025-05-09 14:22:28 -0400

    Debugging

---
+++ python/run_experiment.py
@@ -0,0 +1,11 @@
+def main():
+    while True:
+        print(f"Select the chaos experiment you would like to execute:")
+        print(f"\t1) Terminate process")
+        print(f"\t2) Resource exhaustion")
+        print(f"\t0) Exit")
+        input
+
+
+if __name__ == "__main__":
+    main()
